6931842b36714043775eaa724eae14d22801a966
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-12-16 21:45:41 +0530

    some chnages

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,15 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerController = void 0;
+const db_1 = __importDefault(require("../config/db"));
 const registerController = (req, res) => {
     const { name, email, password } = req.body;
-    res.status(200).json({
-        data: {
-            name: name,
-            email: email,
-            password: password
-        },
+    const userExists = db_1.default.user.findUnique({
+        where: { email: email }
     });
+    if (!userExists) {
+        res.status(200).json({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+        });
+    }
+    else {
+        res.status(400).json({
+            message: "User Already Exists"
+        });
+    }
 };
 exports.registerController = registerController;
 //# sourceMappingURL=user.controller.js.map
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,26 @@
+import prisma from "../config/db";
 import { registerservice } from "../services/user.services";
 import { Request, Response } from "express";
 
 export const registerController = (req: Request, res: Response) => {
-    const {name,email,password} = req.body;
-    res.status(200).json({
-        data: {
-            name:name,
-            email:email,
-            password:password
-        },
+    const { name, email, password } = req.body;
+    const userExists = prisma.user.findUnique({
+        where: { email: email }
     })
+    if (!userExists) {
+        res.status(200).json({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+        })
+    }
+    else {
+        res.status(400).json({
+            message: "User Already Exists"
+        })
+    }
 
 
 }
